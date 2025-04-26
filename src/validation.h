8f153885d649189049c3616358fd1139791a615a
jephthah-orobia <jephthah.orobia@gmail.com>
2025-04-25 21:05:01 -0500

    change parameter name to correct convention, document the function getValidInput

--- src/validation.h
+++ src/validation.h
@@ -16,14 +16,23 @@ using namespace std;
 
 namespace validation
 {
+    /**
+     * returns a valid value based on type and the parameter `is_valid`
+     * Parameters:
+     *  - `in`: an instance of `std::istream` like `std::cin`
+     *  - `out`: an instance of `std::ostream` like `std::cout`
+     *  - `prompt`:<string> message/prompt user to input
+     *  - `err_msg`: error message to display when user inputs an invalid value
+     *  - `is_valid`: a function to check if the captured value is valid.
+     */
     template <typename T>
-    T getValidInput(istream &in, ostream &out, string prompt, string err_msg, function<bool(T)> isValid)
+    T getValidInput(istream &in, ostream &out, string prompt, string err_msg, function<bool(T)> is_valid)
     {
         int n;
         out << prompt;
         in >> n;
         in.ignore(numeric_limits<streamsize>::max(), '\n');
-        if (!in.fail() && isValid(n))
+        if (!in.fail() && is_valid(n))
             return n;
         else
         {
